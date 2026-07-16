f35b41b0a6c114ec680ff90ba700f3a6ffd6ebdc
Kirill Siukhin <kirillsiukhin@proton.me>
2026-07-16 16:00:54 +0500

    refactor: move providers to layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,14 +1,20 @@
 import { Outlet } from 'react-router';
 import { AppSidebar } from '@/components/sidebar/app-sidebar';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { TooltipProvider } from './components/ui/tooltip';
+import { SolveProvider } from './contexts/solve/solve-provider';
 
 export function Layout() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Outlet />
-      </SidebarInset>
-    </SidebarProvider>
+    <TooltipProvider>
+      <SolveProvider>
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <Outlet />
+          </SidebarInset>
+        </SidebarProvider>
+      </SolveProvider>
+    </TooltipProvider>
   );
 }
