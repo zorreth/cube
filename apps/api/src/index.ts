64723b01e06ce97f2c89b1cd10dbea5c5eaa28d1
Kirill Siukhin <kirillsiukhin@proton.me>
2026-07-17 03:29:34 +0500

    chore: prepare fastify and auth plugin

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello via Bun!');
+import Fastify from 'fastify';
+import { authRoutes } from './routes/auth';
+
+const fastify = Fastify({ logger: true });
+
+fastify.register(authRoutes, { prefix: '/api/auth' });
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
---
+++ apps/api/src/routes/auth.ts
@@ -0,0 +1,3 @@
+import type { FastifyInstance } from 'fastify';
+
+export async function authRoutes(fastify: FastifyInstance) {}
