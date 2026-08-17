1e13bac2f8782413858437ef898c0e9cb59f3fc7
Kirill Siukhin <kirillsiukhin@proton.me>
2026-08-17 22:49:40 +0500

    chore(web): add shadcn ui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Nunito_Sans } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const nunitoSans = Nunito_Sans({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en" className={cn('h-full', 'antialiased', 'font-sans', nunitoSans.variable)}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
